15c3251b706f5efbb6e2d713de068b6b48b1356f
Jishenshen <jishenshen97@gmail.com>
2020-05-27 22:55:34 +0100

    fetch

--- 06-Type Ahead/utilize.js
+++ 06-Type Ahead/utilize.js
@@ -1,2 +1,8 @@
 const areaData =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+
+const cities = []
+
+const prom = fetch(areaData)
+  .then(blob => blob.json())
+  .then(data => console.log(data))
