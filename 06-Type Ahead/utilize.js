2c255c2ebfbdac992690c59af75e02a11736ca0e
Jishenshen <jishenshen97@gmail.com>
2020-05-28 21:33:50 +0100

    day06 complete

--- 06-Type Ahead/utilize.js
+++ 06-Type Ahead/utilize.js
@@ -1,45 +1,46 @@
 const areaData =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
+//fetch data
+
 const cities = []
 
-const prom = fetch(areaData)
+fetch(areaData)
   .then(blob => blob.json())
   .then(data => {
     cities.push(...data)
     // console.log(cities)
   })
+//result generator
 
-function findMatches (inputWords, cities) {
+function matchResult (inputValue, cities) {
   return cities.filter(place => {
-    let regex = new RegExp(inputWords, 'gi')
-    return place.city.match(regex) || place.state.match(regex)
+    let regExp = new RegExp(inputValue, 'gi')
+    return place.city.match(regExp) || place.state.match(regExp)
   })
 }
-
-function displayMatches () {
-  // console.log(this.value)
-  const matchArray = findMatches(this.value, cities)
+function displayResult () {
+  const matchArray = matchResult(this.value, cities)
   const html = matchArray
     .map(place => {
-      let regex = new RegExp(this.value, 'gi')
+      let regExp = new RegExp(this.value, 'gi')
       let cityName = place.city.replace(
-        regex,
+        regExp,
         `<span class='hl'>${this.value}</span>`
       )
       return `
-      <li>
-      <span>${cityName},${place.state}</span>
-      <span class = 'population'>${place.population}</span>
-      </li>
-      `
+    <li>
+    <span>${cityName},${place.state}</span>
+    <span class='population'>${place.populatin}</span>
+    </li>`
     })
     .join('')
   suggestions.innerHTML = html
 }
 
-const searchs = document.querySelector('.input')
+//eventListener
+const inputs = document.querySelector('.input')
 const suggestions = document.querySelector('.suggestion')
 
-searchs.addEventListener('change', displayMatches)
-searchs.addEventListener('keyup', displayMatches)
+inputs.addEventListener('change', displayResult)
+inputs.addEventListener('keyup', displayResult)
