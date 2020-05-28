2d451190c909b34b8e7547d45501afaee7a10c3b
Jishenshen <jishenshen97@gmail.com>
2020-05-28 14:25:36 +0100

    day06 complete

--- 06-Type Ahead/utilize.js
+++ 06-Type Ahead/utilize.js
@@ -5,4 +5,41 @@ const cities = []
 
 const prom = fetch(areaData)
   .then(blob => blob.json())
-  .then(data => console.log(data))
+  .then(data => {
+    cities.push(...data)
+    // console.log(cities)
+  })
+
+function findMatches (inputWords, cities) {
+  return cities.filter(place => {
+    let regex = new RegExp(inputWords, 'gi')
+    return place.city.match(regex) || place.state.match(regex)
+  })
+}
+
+function displayMatches () {
+  // console.log(this.value)
+  const matchArray = findMatches(this.value, cities)
+  const html = matchArray
+    .map(place => {
+      let regex = new RegExp(this.value, 'gi')
+      let cityName = place.city.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      )
+      return `
+      <li>
+      <span>${cityName},${place.state}</span>
+      <span class = 'population'>${place.population}</span>
+      </li>
+      `
+    })
+    .join('')
+  suggestions.innerHTML = html
+}
+
+const searchs = document.querySelector('.input')
+const suggestions = document.querySelector('.suggestion')
+
+searchs.addEventListener('change', displayMatches)
+searchs.addEventListener('keyup', displayMatches)
