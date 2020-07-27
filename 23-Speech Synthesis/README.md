48f8897f11659a9fad5c8312dd03e694d02e2a7b
Jishenshen <jishenshen97@gmail.com>
2020-07-27 22:09:26 +0100

    day23 complete

--- 23-Speech Synthesis/README.md
+++ 23-Speech Synthesis/README.md
@@ -2,6 +2,15 @@
 
 第 23 天要做一个语音的记事本类似的场景，输入一段内容，选择不同的语言可以进行朗读。还可以选择不同的语速和语调。
 
+## 思路
+
+首先我们来明确一下这样的效果思路应该如何走
+
+- 设置语言下拉框，并将所选择的语言设置为语音的语言
+- 监听语调和语速的滑动条
+- 可以修改 textarea 的内容
+- 监听开始和暂停按钮
+
 ## css
 
 1. 针对 button 点击会有移动的效果，此案例将 button 在 active 情况下的 top 值改变，但是要注意他需要是`position: relative`,如果是 display 的排布方式，无法正常使用
@@ -13,3 +22,9 @@ button:active {
 ```
 
 ## JavaScript
+
+- `SpeechSynthesis.getVoices()`：获取所有的语言列表，代表在当前语音对象上所有可用的语言；
+- `SpeechSynthesis.cancel()`：结束，结束当前的语音状态，并将当前语音内容清空；
+- `SpeechSynthesis.pause()`：暂停，暂停当前的语音状态，当不清空语音内容，可以继续播放；
+- `SpeechSynthesis.speak()`：播放，将文字内容加入到播放序列中并开始播放语音；
+- `SpeechSynthesis.resume()`：继续，当语音处于暂停状态的时候，继续播放该语音；
--- 23-Speech Synthesis/utilize.js
+++ 23-Speech Synthesis/utilize.js
@@ -6,23 +6,47 @@ const stopButton = document.getElementById('stop')
 const speakButton = document.getElementById('speak')
 msg.text = document.querySelector('[name="text"]').value
 
-function speakMsg () {}
-function stopMsg () {}
-
+//获取人声
 function populateVoiceList () {
-  let voices = this.getVoices()
+  voices = this.getVoices()
   voicesDropdown.innerHTML = voices
-    .filter(voice => {
-      voice.lang.includes('en')
-    })
     .map(
       voice =>
-        `<option value = "${voice.name}"> ${voice.name}(${voice.lang})></option>`
+        `<option value='${voice.name}'>${voice.name}(${voice.lang})</option>`
     )
-  // .join('')
+    .join('')
+  console.log(this)
 }
 
-speakButton.addEventListener('click', speakMsg)
-stopButton.addEventListener('click', stopMsg)
+//设置人声
+function setVoice () {
+  msg.voice = voices.find(voice => voice.name === this.value)
+  toggle()
+}
 
+//每次切换语言时都从头重新开始播放
+function toggle (startOver = true) {
+  speechSynthesis.cancel()
+  if (startOver) {
+    speechSynthesis.speak(msg)
+  }
+}
+
+//设定速率和音准
+function setOption () {
+  msg[this.name] = this.value
+  toggle()
+  console.log(msg)
+}
+
+// 设置语言下拉选择框
 speechSynthesis.addEventListener('voiceschanged', populateVoiceList)
+
+// 当切换语言选择下拉菜单时被调用
+voicesDropdown.addEventListener('change', setVoice)
+
+//监听rate和pitch控制条
+options.forEach(option => option.addEventListener('change', setOption))
+
+speakButton.addEventListener('click', toggle)
+stopButton.addEventListener('click', () => toggle(false))
